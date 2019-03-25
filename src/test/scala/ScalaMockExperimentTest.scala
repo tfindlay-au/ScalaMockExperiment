bda497b39f63c9df2012d06b2114ed060933c0fd
timothyfindlay <tfindlay@prodevelop.com.au>
2019-03-26 00:02:19 +1100

    Adjustment

--- src/test/scala/ScalaMockExperimentTest.scala
+++ src/test/scala/ScalaMockExperimentTest.scala
@@ -3,13 +3,13 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class ScalaMockExperimentTest extends FlatSpec with Matchers with MockFactory {
 
-  "ScalaMockExperiment.doSomething(false)" should "return Foo Bar" in {
+  "ScalaMockExperiment.doSomething(false)" should "return Goodbye World" in {
     val result = ScalaMockExperiment.doSomething(false)
     result shouldBe "Goodbye World"
   }
 
   "ScalaMockExperiment.doSomething(true)" should "return Foo Bar" in {
-    val myMockTrait = stub[MyTrait]
+    val myMockTrait = mock[MyTrait]
 
     (myMockTrait.GetMessage _) expects() returns("Foo Bar")
 
